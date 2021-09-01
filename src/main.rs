042fc07605f1e8c36e385061a1d72c548df57a12
rayasm1 <mahesh.rayas@anz.com>
2021-09-01 13:24:45 +1000

    pass issue as args

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 
 use std::env;
+use reqwest::header::HeaderMap;
 
 #[tokio::main]
 async fn main() -> Result<(), reqwest::Error> {
@@ -15,11 +16,27 @@ async fn main() -> Result<(), reqwest::Error> {
     // get all the comments by user.login = user passed "github-actions[bot]" and get the id and 
     // repos/anzx/platform-secrets-management/pulls/comments/{comment_id}
 
-    println!("INPUT_ORG {:?}",env::var("INPUT_ORG").unwrap());
+    println!("INPUT_ORG {:?}",env::var("INPUT_REPO").unwrap());
     println!("printing env {:?}",env::var("INPUT_REPO").unwrap());
     println!("printing INPUT_USER env {:?}",env::var("INPUT_USER").unwrap());
+    println!("printing issue env {:?}",env::var("INPUT_ISSUE").unwrap());
 
-    let res = reqwest::get("https://hyper.rs").await?;
+
+    let client = reqwest::Client::new();
+
+    let mut headers = HeaderMap::new();
+    let token= format!("token {}",env::var("INPUT_GITHUB_TOKEN").unwrap());
+    headers.insert("Authorization", token.parse().unwrap());
+    headers.insert("Accept", "application/vnd.github.VERSION.full+json".parse().unwrap());
+
+    let repo = env::var("INPUT_REPO").unwrap();
+    let org = env::var("INPUT_REPO").unwrap();
+    let user = env::var("INPUT_USER").unwrap();
+    let issue = env::var("INPUT_ISSUE").unwrap();
+
+    let url = format!("https://api.github.com/repos/{}/{}/issues/{}/comments",&org,&repo,&issue);
+
+    let res = client.get(url).headers(headers).send().await?;
 
     println!("Status: {}", res.status());
 
