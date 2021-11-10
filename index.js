d618b62a55f602353983722b800bd507f63cb689
Mahesh Rayas <maheshrayas@gmail.com>
2021-11-10 12:47:43 +1100

    fix request

--- index.js
+++ index.js
@@ -4,23 +4,37 @@ const { Octokit } = require("@octokit/action");
 async function run() {
 try {
     const githubToken = core.getInput('github_token');
-    const org = core.getInput('org');
+    const owner = core.getInput('org');
     const user = core.getInput('user');
     const issue = core.getInput('issue');
     const repo = core.getInput('repo');
-
+    
+    const octokit = new Octokit();
     let token = `token ${githubToken}` 
-    let req = await Octokit.request('GET /repos/{owner}/{repo}/issues/{issue_number}/comments', {
+    let req = await octokit.request('GET /repos/{owner}/{repo}/issues/{issue_number}/comments', {
         headers: {
             authorization: token,
+            Accept: "application/vnd.github.v3+json"
           },
-        owner: org,
-        repo: repo,
+        owner,
+        repo,
         issue_number: issue
       })
+    for ( const issue of req.data){
+       if (issue.user.login == user) {
+        await octokit.request('DELETE /repos/{owner}/{repo}/issues/comments/{comment_id}', {
+            headers: {
+                authorization: token,
+                Accept: "application/vnd.github.v3+json"
+              },
+              owner,
+              repo,
+              comment_id: `${issue.id}`
+          })
+          console.log(`Successfully deleted /repos/{owner}/{repo}/issues/comments/${issue.id}`)
+       }
 
-    console.log(`${result.data.length} repos found.`);
-
+    }
   } catch (error) {
     core.setFailed(error.message);
   }
