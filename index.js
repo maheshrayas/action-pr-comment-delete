24834cba0060bec03611a4647c2b4f43196b56d9
Mahesh Rayas <maheshrayas@gmail.com>
2021-11-10 11:52:46 +1100

    Support mac and windows

---
+++ index.js
@@ -0,0 +1,28 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const { Octokit } = require("@octokit/action");
+async function run() {
+try {
+    const githubToken = core.getInput('github_token');
+    const org = core.getInput('org');
+    const user = core.getInput('user');
+    const issue = core.getInput('issue');
+    const repo = core.getInput('repo');
+
+    let token = `token ${githubToken}` 
+    let req = await octokit.request('GET /repos/{owner}/{repo}/issues/{issue_number}/comments', {
+        headers: {
+            authorization: token,
+          },
+        owner: org,
+        repo: repo,
+        issue_number: issue
+      })
+
+    console.log(`${result.data.length} repos found.`);
+
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+run();
